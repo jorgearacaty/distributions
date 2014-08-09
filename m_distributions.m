4a5fe9ba3a53caa1021b88200c60b0bda91d79a5
jorgearacaty <jorgearacaty@gmail.com>
2014-08-08 22:13:23 -0300

    jorgearacaty 003 20140808

--- m_distributions.m
+++ m_distributions.m
@@ -1,27 +1,43 @@
 % ---------------------------------------
-% --- Programa Matlab.
-% --- Fit dezena 2 da quina para Rayleigh() e outras.
-% --- jorgearacaty 31 jul 2014 - 11:07.
+% --- Matlab 7.9.0.529 (R2009b).
+% --- m_distributions.m - fit dezena 2 da quina,
+% --- para Rayleigh() e outras.
+% --- criado em - jorgearacaty, 31 jul 2014 - 11:07.
+% --- jorgearacaty, 8 aug 2014 - 2211.
 % ---------------------------------------
 
+% -------------------------------
+% Splash.
+% -------------------------------
+clear; clc;
+nome_programa = 'm_distributions.m';
+fprintf('\n------------------------------------------');
+fprintf('\n--- programa: %s',nome_programa);
+fprintf('\n------------------------------------------\n');
+
+% File operations, lê CSV, ordena linhas, matrix dimensions.
 file_name = 'csvQuina.csv'; I = csvread(file_name);
 Q = sort(I,2); 
 [mQ, nQ] = size(Q);
 
 figure;
+
+% histograma da segunda dezena.
 hist(Q(:,2),80);
 hold on;
-% --- Rayleigh distribution, plota pdf entre 1 e 80.
+
+% Rayleigh distribution, plota pdf.
 pdfRay = raylpdf([1:80],21.5019);
 plot([1:80],pdfRay*mQ,'g*');
-guessRay = round(raylrnd(21.4913))
 
-% --- Weibull distribution
+% palpite ilustrativo.
+%guessRay = round(raylrnd(21.4913))
+
+% Weibull distribution, plota pdf.
 pdfWbl = wblpdf([1:80],30.6001,2.0633);
+plot([1:80],pdfWbl*mQ,'r.');  % pdfWbl*mQ é para manter o gráfico aceitável.
 
-% figure;
-plot([1:80],pdfWbl*mQ,'r.');
-% pdfWbl*mQ é a dica para manter o gráfico aceitável.
+% titulos e legendas.
 title('Distribuições para a segunda dezena da quina') 
 legend('histograma','Rayleigh','Weibull','Location','NorthEast');
 xlabel('x');
@@ -30,36 +46,40 @@ ylabel('y');
 % ---------------------------------------------
 % prova do CLT para a primeira dezena da quina.
 % ---------------------------------------------
-figure
+figure;
+
+% histograma da distribuição da primeira dezena
 P = Q(:,1);
 hist(P);
 
 mu = mean(P)
 sigma = std(P);
-n = 30;                 % tamanho das amostras.
-q_samples = 30;          % quantidade de amostras.  
+
+% tamanho das amostras.
+n = 30;
+
+% quantidade de amostras. 
+q_samples = 30;  
+
+% standard deviation calculado (sample means).
 sigma_bar = sigma/sqrt(n)
 
-% recolhe amostras.
+% amostragem.
 A = zeros(q_samples, n);
 for ind = 1:q_samples
     A(ind,:) = randsample(P,n);
 end
-% medias das amostras
+
+% medias das amostras.
 samples_means = mean(A');
+
 % parametros das amostras.
 clt_mean_bar = mean(samples_means)
 clt_sigma_bar = std(samples_means)
 
-figure
+figure;
+
+% histograma das samples means.
 hist(samples_means)
 
-
-
-% ---------------------------------
-if 1 == 2
-    figure;
-    pdfWbl = wblpdf([1:80],55,3.1123);
-    plot([1:80],pdfWbl,'y.');
-end
 
