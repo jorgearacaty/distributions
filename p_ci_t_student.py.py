5a9a29d3f92086cf91439d9d61f2ec27310440f5
jorgearacaty <jorgearacaty@gmail.com>
2014-08-11 13:59:32 -0300

    jorgearacaty 001 20140811

--- p_ci_t_student.py.py
+++ p_ci_t_student.py.py
@@ -1,17 +1,14 @@
 ﻿# --------------------------------------------
 # --- Python 2.7.3.
-# --- confidence_interval.py - fit dezena 2 da quina para Rayleigh().
-# --- criado em - jorgearacaty 10 aug 2014 - 0214.
-# --- jorgearacaty, 10 aug 2014 - 2021.
+# --- p_ci_t_student.py - fit dezena 2 da quina para Rayleigh().
+# --- criado em - jorgearacaty 10 aug 2014 - 2300.
+# --- jorgearacaty, 11 aug 2014 - 1345.
 # --------------------------------------------
 
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy
-import scipy.stats as scs
 from scipy.stats import norm
-#import matplotlib.mlab as mlab
-#from scipy import stats
 from scipy.stats import t
 from scipy import stats 
 
@@ -27,144 +24,95 @@ import math
 def confidence_interval():
 
     # --------------------------------------
+    # 95% confidence interval - t Student.    
     # sigma unknown - Anderson 2008 pag 310.
-    # 95% confidence interval - t Student.
     # http://adventuresinpython.blogspot.com.br/2012/12/confidence-intervals-in-python.html
 
-    # Levine pag 292, exercício 8.14 
+    # Levine pag 292, exercício 8.14.
     s = np.array([1, 2, 3, 4, 5, 6, 20])
-    
+
+    # extrai parametros do vetor.
     n, min_max, mean, var, skew, kurt = stats.describe(s)
-    
+    print '\ndiferente, var extraida por stats.describe(s) e np.var(s)'
+    print 'denominador (n-1) = ', str(var), ', denominador (n) = ', str(np.var(s)), '\n'
+
+    # set parametros.
     s_amostra = math.sqrt(var)
     n_amostra = n
     mu_amostra = mean
     alpha = 0.05
-    
-    # n_amostra = 7
 
-
-    tinf = scs.t.ppf(alpha/2,n_amostra-1)
+    # calcula os t scores para os pontos críticos,
+    # percent point function (ppf), dada uma cdf calcula o ppf,
+    # ou seja, o valor x do ponto na Student curve
+    # (n_amostra - 1) é o grau de liberdade.
+    # norm.ppt equivale a norminv e norm.pdf a normpdf.
+    tinf = t.ppf(alpha/2,n_amostra-1)
     tsup = t.ppf(1-(alpha/2),n_amostra-1)
 
     
-
-    # outro metodo de achar os t scores.
-    #tinf, tsup = t.interval(0.95, n_amostra-1, loc=1, scale=0)
-    #print tinf, tsup, mean, s_amostra, n_amostra
-
-    #print t.pdf(tinf,n_amostra-1)
-
     xis_inf = mu_amostra + (tinf * (s_amostra / math.sqrt(n_amostra)))
     xis_sup = mu_amostra + (tsup * (s_amostra / math.sqrt(n_amostra)))
 
     print xis_inf, xis_sup
 
+    # ---------------------------------------------
+    # desenha curva baseada na distribuição normal.
+    # ---------------------------------------------
     mu = mu_amostra
     sigma = s_amostra
     limite_inferior = xis_inf
     limite_superior = xis_sup
+
     xa = np.linspace(mu-(3.99*sigma),limite_inferior)
     xb = np.linspace(limite_inferior,limite_superior)
     xc = np.linspace(limite_superior,mu+(3.99*sigma))
+    
     x = np.concatenate((xa,xb,xc), axis=0)
 
-    #vcdfa = t.cdf(xa,n_amostra-1)
-    #vppfa = t.ppf(vcdfa,n_amostra-1)
-    #print vppfa, vcdfa
-    #ya = mu_amostra + (vppfa* (s_amostra / math.sqrt(n_amostra)))
-    
     ya = norm.pdf(xa,mu,sigma)
     yb = norm.pdf(xb,mu,sigma)
     yc = norm.pdf(xc,mu,sigma)
-    y = np.concatenate((ya,yb,yc), axis=0)    
+
+    y = np.concatenate((ya,yb,yc), axis=0)
+
     plotar('Confidence Interval','t student scores','x bar', \
            'probabilidade','pdf',x,y,limite_inferior, \
            limite_superior,sigma,mu,xa,ya,xc,yc,alpha,np.array([]))
-
+    
+    # ----------------------------------------------
+    # desenha curva baseada na distribuição Student.
+    # ----------------------------------------------    
     mu = mu_amostra
     sigma = s_amostra
     limite_inferior = tinf
     limite_superior = tsup
+    
     xa = np.linspace(-4,limite_inferior)
     xb = np.linspace(limite_inferior,limite_superior)
     xc = np.linspace(limite_superior,4)
+    
     x = np.concatenate((xa,xb,xc), axis=0)
+    
     ya = t.pdf(xa,n_amostra-1)
     yb = t.pdf(xb,n_amostra-1)
     yc = t.pdf(xc,n_amostra-1)
+    
     y = np.concatenate((ya,yb,yc), axis=0)
 
     za = norm.pdf(xa)
     zb = norm.pdf(xb)
     zc = norm.pdf(xc)
+    
     z = np.concatenate((za,zb,zc), axis=0)
     
     plotar('Confidence Interval','t student scores','x bar', \
                'probabilidade','pdf',x,y,limite_inferior, \
                limite_superior,sigma,mu,xa,ya,xc,yc,alpha,z)
         
-    # ---------------------------------------
-   
-    if 1 == 2:
-        
-        s_pop = 20
-        n =100
-        # confidence interval, media, desvio padrao.
-        alpha, mu, sigma = 0.05, 60, s_pop/math.sqrt(n) 
-        print s_pop/math.sqrt(n) 
-
-        # valores criticos.
-        limite_inferior = norm.ppf(alpha/2, mu, sigma);
-        limite_superior = norm.ppf(1-(alpha/2), mu, sigma);
-
-        # eixo horizontal - com 4 vezes pode dar erro to tamanho do eixo.
-        xa = np.linspace(mu-(3.99*sigma),limite_inferior)
-        xb = np.linspace(limite_inferior,limite_superior)
-        xc = np.linspace(limite_superior,mu+(3.99*sigma))
-
-        x = np.concatenate((xa,xb,xc), axis=0)
-
-        #print xa.min(), x.min()
-
-        # eixo vertical.
-        ya = norm.pdf(xa,mu,sigma)
-        yb = norm.pdf(xb,mu,sigma)
-        yc = norm.pdf(xc,mu,sigma)
-        
-        y = np.concatenate((ya,yb,yc), axis=0)
-  
-        plotar('Confidence Interval','distribuicao','x bar', \
-               'probabilidade','pdf',x,y,limite_inferior, \
-               limite_superior,sigma,mu,xa,ya,xc,yc,alpha)
-
-    if 1 == 2:
-        
-        #sigma = 1
-
-        li_st = norm.ppf(alpha/2);
-        ls_st = norm.ppf(1-(alpha/2));
-
-        #s_pop = 20
-        #n = 100
-        #print (60 + (li_st * s_pop/math.sqrt(n)))
-
-        xa = np.linspace(-4,li_st)
-        xb = np.linspace(li_st,ls_st)
-        xc = np.linspace(ls_st,4)
-
-        x = np.concatenate((xa,xb,xc), axis=0)
-
-        ya = norm.pdf(xa)
-        yb = norm.pdf(xb)
-        yc = norm.pdf(xc)
-        
-        y = np.concatenate((ya,yb,yc), axis=0)
-        
-        plotar('Confidence Interval','z scores','x bar', \
-               'probabilidade','pdf',x,y,li_st, \
-               ls_st,1,0,xa,ya,xc,yc,alpha)
-        
+# -----------------------------------------------
+# desenha gráficos.
+# -----------------------------------------------        
 def plotar(tit_janela,tit_graf,x_lbl,y_lbl,leg_1,x,y, \
            linf,lsup,sigma,mu,xa,ya,xc,yc,alpha,z):
     
@@ -185,8 +133,10 @@ def plotar(tit_janela,tit_graf,x_lbl,y_lbl,leg_1,x,y, \
 
         # "%0.2f" % linf
         # labels valor critico.
-        plt.text(linf, ya.max(),' v. critico (' + "%0.2f" % linf+')',horizontalalignment='right')
-        plt.text(lsup, ya.max(),' v. critico (' + "%0.2f" % lsup+')', horizontalalignment='left')
+        plt.text(linf, ya.max(),' v. critico (' + "%0.2f" % linf+')', \
+                 horizontalalignment='right')
+        plt.text(lsup, ya.max(),' v. critico (' + "%0.2f" % lsup+')', \
+                 horizontalalignment='left')
 
         # label alpha/2 inferior.
         plt.text(xa.min()+((linf-xa.min())/2), ya.max()/2, \
@@ -222,7 +172,7 @@ def plotar(tit_janela,tit_graf,x_lbl,y_lbl,leg_1,x,y, \
         # desenha curva em forma de sino.
         plt.plot(x,y,'r-', label = 't scores')
         if len(z) > 0:
-            plt.plot(x,z,'b*', label = 'z scores')
+            plt.plot(x,z,'b.', label = 'z scores')
 
         # áreas anchuradas de alpha/2.
         plt.fill_between(xa,ya,0,color='cyan')
@@ -253,4 +203,6 @@ if __name__ == '__main__':
     confidence_interval()
     
     print "\nDONE"
+    The_End = raw_input("press any key to go out, thks.\n")
+
 
